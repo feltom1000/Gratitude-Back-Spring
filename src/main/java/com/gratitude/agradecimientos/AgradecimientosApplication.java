74355ee62e63d9b34ecde25cae2dfa1d1fbe8899
feltom1000 <feltom1000@hotmail.com>
2022-01-24 17:35:56 -0300

    inicio de proyecto

---
+++ src/main/java/com/gratitude/agradecimientos/AgradecimientosApplication.java
@@ -0,0 +1,13 @@
+package com.gratitude.agradecimientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgradecimientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgradecimientosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gratitude/agradecimientos/AgradecimientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gratitude.agradecimientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgradecimientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
