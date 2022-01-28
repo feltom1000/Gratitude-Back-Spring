e712bd056c6a2fd5ce76042de41e553d1f1432c5
feltom1000 <feltom1000@hotmail.com>
2022-01-28 12:05:42 -0300

    Principales funcionalidades de BackEnd terminadas

--- src/main/java/com/gratitude/agradecimientos/domain/ThanksPost.java
+++ src/main/java/com/gratitude/agradecimientos/domain/ThanksPost.java
@@ -3,8 +3,22 @@ package com.gratitude.agradecimientos.domain;
 import java.util.Date;
 
 public class ThanksPost {
-    private int idPost;
-    private int idUser;
-    private Date fecha;
-    private int idThanks;
+    private int postId;
+    private int thanksId;
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getThanksId() {
+        return thanksId;
+    }
+
+    public void setThanksId(int thanksId) {
+        this.thanksId = thanksId;
+    }
 }
